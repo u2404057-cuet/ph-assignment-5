ceaaec9c87d3c43f81ef2454e99c4bd25c3853e7
u2404057-cuet <u2404057@student.cuet.ac.bd>
2026-03-09 07:17:55 +0600

    done

--- script/login.js
+++ script/login.js
@@ -1,4 +1,4 @@
-document.getElementById('login-btn').addEventListener('click', () => {
+document.getElementById("login-btn").addEventListener('click', () => {
     const userName = document.getElementById('username-input').value;
     const password = document.getElementById('password-input').value;
 
